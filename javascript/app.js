e85f4066278055a185bfe493a07b47efe326afdb
Cyndi Norwood <cyndinorwood@me.com>
2018-01-30 11:35:23 -0800

    tidy up

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,6 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
-let missed   = 0;
+let missed = 0;
 
 const resetButton = document.querySelector('.btn__reset');
 
